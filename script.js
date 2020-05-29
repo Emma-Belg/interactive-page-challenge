852ba15bead1e00ddc865c24505b59e67f9710a7
Emma-Belg <57953767+Emma-Belg@users.noreply.github.com>
2020-05-29 10:45:27 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var selectedImg = document.getElementById(‘selected_img’);
- var images = document.getElementById(‘image_list’).getElementsByTagName(‘li’);for (i = 0; i < images.length; i++)
- {
- images[i].addEventListener(‘click’, activateImage);
- }function activateImage()
- {
- selectedImg.innerHTML = this.innerHTML;
- }
