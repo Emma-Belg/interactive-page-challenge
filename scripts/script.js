3aafe73df94ca3bc2678e5bf4308263f157bab58
emma <emma.wishart@live.com.au>
2020-05-29 12:15:20 +0200

    working on carousel

--- scripts/script.js
+++ scripts/script.js
@@ -49,7 +49,8 @@ function disableFormSend() {
 //https://stackoverflow.com/questions/20040825/check-how-many-li-there-are-in-a-ul-with-javascript
 function imageSlider() {
     let selectedImg = document.getElementById('selected_img');
-    let images = document.getElementById('image_list').getElementsByTagName('li');for (i = 0; i < images.length; i++)
+    let images = document.getElementById('image_list').getElementsByTagName('li');
+    for (let i = 0; i < images.length; i++)
     {
         images[i].addEventListener('click', activateImage);
     }function activateImage()
@@ -57,6 +58,55 @@ function imageSlider() {
         selectedImg.innerHTML = this.innerHTML;
     }
 }
+
+//image carousel
+(function (doc) {
+    let itemClassName = "carousel__photo";
+    let items = d.getElementsByClassName(itemClassName);
+    let totalItems = items.length;
+    let slide = 0;
+    let moving = true;
+
+    // Set classes
+    function setInitialClasses() {
+        // Targets the previous, current, and next items
+        // This assumes there are at least three items.  items[totalItems - 1].classList.add("prev");
+        items[0].classList.add("active");
+        items[1].classList.add("next");
+    }// Set event listeners
+    function setEventListeners() {
+        var next = d.getElementsByClassName('carousel__button--next')[0],
+            prev = d.getElementsByClassName('carousel__button--prev')[0];  next.addEventListener('click', moveNext);
+        prev.addEventListener('click', movePrev);
+    }
+
+
+    // Next navigation handler
+    function moveNext() {  // Check if moving
+        if (!moving) {    // If it's the last slide, reset to 0, else +1
+            if (slide === (totalItems - 1)) {
+                slide = 0;
+            } else {
+                slide++;
+            }    // Move carousel to updated slide
+            moveCarouselTo(slide);
+        }
+    }// Previous navigation handler
+    function movePrev() {  // Check if moving
+        if (!moving) {    // If it's the first slide, set as the last slide, else -1
+            if (slide === 0) {
+                slide = (totalItems - 1);
+            } else {
+                slide--;
+            }
+
+            // Move carousel to updated slide
+            moveCarouselTo(slide);
+        }
+    }
+
+    
+})(document);
 
 
 
