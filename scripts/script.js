fec6b9600d82dd2838b980071773994137413a80
emma <emma.wishart@live.com.au>
2020-05-29 14:54:59 +0200

    basic counters work

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,36 @@
 
-/*//onload="visitorsCounter()";
+//onload="visitorsCounter()";
 
 
 //document.getElementById("visitorsCounter").addEventListener("load", visitorsCounter);
 
-function visitorsCounter() {
-    document.getElementById("demo").innerHTML = "Iframe is loaded.";
+let startNumber = 0;
+const totalVisitors = 50000;
+let totalGoals = 20;
+let totalFouls = 50
 
-    let startNumber = 0;
-    const endNumber = 50000;
-
+function counter(endNumber, id) {
     //https://codepen.io/vajkri/pen/VJKgRa
-    let startNr = 0
-    const endNr = 20
 
-    document.querySelector('h1').innerHTML = startNr
+    document.getElementById(id).innerHTML = startNumber;
 
     const countUp = (count) => {
-        document.querySelector('h1').innerHTML = count
+        document.getElementById(id).innerHTML = count
 
-        if(count < endNr) {
-            setTimeout(() => countUp(count + 1), 60)
+        if(count < endNumber) {
+            setTimeout(() => countUp(count + 1), 600)
         }
+        return count
     }
+    document.getElementById(id).innerHTML = countUp(1);
 
-    const reset = () => {
-        startNr = 0
-        document.querySelector('h1').innerHTML = startNr
-    }
-
-    document.querySelector('#countBtn').addEventListener('click', () => countUp(startNr))
-    document.querySelector('#resetBtn').addEventListener('click', reset)
+    document.getElementById(id).addEventListener("load", counter);
+    //document.querySelector('#resetBtn').addEventListener('click', reset)
 }
+counter(totalVisitors, "visitors");
+counter(totalGoals, "goals");
+counter(totalFouls, "faulths");
 
- */
 
 function disableFormSend() {
     //
@@ -50,64 +47,18 @@ disableFormSend();
 
 //https://stackoverflow.com/questions/20040825/check-how-many-li-there-are-in-a-ul-with-javascript
 
-let carouselImgs = document.querySelectorAll(".carousel");
-let arrowRight =document.querySelector(".carousel__button--prev");
-let arrowLeft =  document.querySelector(".carousel__button--next");
-let current = 0;
 
 function imageSlider() {
-//hide all images
-    function reset() {
-        for(let i  = 0; i< carouselImgs.length;i++){
-            carouselImgs[i].style.display="none"
-        }
-    }
 
-//call the reset, and only show first one
-    function startSlide() {
-        //first you hide them all
-        reset();
-        //then, show the first image
-        carouselImgs[0].style.display = 'block';
-    }
-    startSlide();
-
-//show prev
-    function  slideLeft() {
-        reset();
-        carouselImgs[current -1].style.display ="block";
-        current --;
-    }
-//show next
-    function  slideRight() {
-        reset();
-        carouselImgs[current +1].style.display ="block";
-        current ++;
-    }
-
-    arrowLeft.addEventListener("click", function () {
-        if(current === 0){
-            current =  carouselImgs.length;
-        }
-        slideLeft();
-    });
-
-    arrowRight.addEventListener("click", function () {
-        if(current === carouselImgs.length -1){
-            current = -1;
-        }
-        slideRight();
-    });
 }
 imageSlider();
 
 //image carousel
-/*!(function(d){
-    // Variables to target our base class, get carousel items,
-    // count how many carousel items there are set the slide to
-    // 0 (which is the number that tells us the frame we're on), and
-    // set motion to true which disables interactivity.
-    var itemClassName = "carousel";
+
+
+!(function(d){
+    // Variables to target our base class,  get carousel items, count how many carousel items there are, set the slide to 0 (which is the number that tells us the frame we're on), and set motion to true which disables interactivity.
+    var itemClassName = "carousel__photo";
     items = d.getElementsByClassName(itemClassName),
         totalItems = items.length,
         slide = 0,
@@ -124,6 +75,7 @@ imageSlider();
     }
 
     // Set click events to navigation buttons
+
     function setEventListeners() {
         var next = d.getElementsByClassName('carousel__button--next')[0],
             prev = d.getElementsByClassName('carousel__button--prev')[0];
@@ -133,13 +85,13 @@ imageSlider();
     }
 
     // Disable interaction by setting 'moving' to true for the same duration as our transition (0.5s = 500ms)
-/!*    function disableInteraction() {
+    function disableInteraction() {
         moving = true;
 
         setTimeout(function(){
             moving = false
         }, 500);
-    }*!/
+    }
 
     function moveCarouselTo(slide) {
 
@@ -176,8 +128,8 @@ imageSlider();
                     oldNext = 1;
                 }
 
-                // Now we've worked out where we are and where we're going,
-                // by adding and removing classes, we'll be triggering the carousel's transitions.
+                // Now we've worked out where we are and where we're going, by adding and removing classes, we'll be triggering the carousel's transitions.
+
                 // Based on the current slide, reset to default classes.
                 items[oldPrevious].className = itemClassName;
                 items[oldNext].className = itemClassName;
@@ -192,13 +144,17 @@ imageSlider();
 
     // Next navigation handler
     function moveNext() {
+
+        // Check if moving
         if (!moving) {
+
             // If it's the last slide, reset to 0, else +1
             if (slide === (totalItems - 1)) {
                 slide = 0;
             } else {
                 slide++;
             }
+
             // Move carousel to updated slide
             moveCarouselTo(slide);
         }
@@ -206,27 +162,37 @@ imageSlider();
 
     // Previous navigation handler
     function movePrev() {
+
+        // Check if moving
         if (!moving) {
+
             // If it's the first slide, set as the last slide, else -1
             if (slide === 0) {
                 slide = (totalItems - 1);
             } else {
                 slide--;
             }
+
             // Move carousel to updated slide
             moveCarouselTo(slide);
         }
     }
+
     // Initialise carousel
     function initCarousel() {
         setInitialClasses();
         setEventListeners();
+
         // Set moving to false now that the carousel is ready
         moving = false;
     }
 
+    // make it rain
     initCarousel();
-}(document));*/
+
+}(document));
+
+
 
 
 
